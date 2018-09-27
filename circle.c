5b86082081b3bacb5ced6eaa4f7cf1d3427a34c4
lohiteshkn <lohitesh.kn@cittumkur.org>
2018-09-27 20:37:05 +0530

    My First Edit

---
+++ circle.c
@@ -0,0 +1,13 @@
+//Program to find the area of a circle
+//Fill the blanks
+# include<stdio.h>
+# include<math.h>
+#define pi 3.142
+void main()
+{
+float r,a;
+printf("enter the value of r");
+________________
+a=2*pi*r;
+printf("area=%f",a);
+}
---
+++ rectangle.c
@@ -0,0 +1,13 @@
+//Program to find area of a rectangle
+ //Fill the dash line(______)
+
+# include<stdio.h>
+void main()
+{
+int len,bre,area;
+
+printf("enter value of len & bre");
+scanf("%d%d",&len,&bre);
+area=______ ;
+printf("Area of a rectangle=______\n",______);   
+}
